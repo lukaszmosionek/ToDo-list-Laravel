e8ae3e339cff1d3e81aec8aa5ecff13d6dddc63a
lukaszmosionek <mosioneklukasz@gmail.com>
2025-03-23 20:52:46 +0100

    Google Calendar Integration

---
+++ resources/views/mail/task/notification.blade.php
@@ -0,0 +1,15 @@
+<x-mail::message>
+# Reminder
+
+Twoje zadanie {{ $task->name }} jutro się kończy.
+
+{{--  <x-mail::button :url="''">
+Button Text
+</x-mail::button>  --}}
+
+
+
+
+{{--  Thanks,<br>
+{{ config('app.name') }}  --}}
+</x-mail::message>
